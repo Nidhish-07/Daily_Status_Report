c864a7c0ab4ef4f1224f2129c99e126db2e03bb2
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-06 16:18:19 +0530

    Registration functionalities

--- daily_status_repor/src/pages/Landing.jsx
+++ daily_status_repor/src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
+import { signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../firebase.js"
 import { useNavigate } from "react-router-dom"
 
@@ -8,6 +8,8 @@ const Landing = () => {
     //Fields for login form
     const [email, setEmail] = React.useState("")
     const [password, setPassword] = React.useState("")
+    const [registering, setRegistering] = React.useState("")
+    const [registrationInfo, setRegistrationInfo] = React.useState({ password: "", confirmPassword: '' })
 
     //To make user login persistance
     React.useEffect(() => {
@@ -37,14 +39,72 @@ const Landing = () => {
         signInWithEmailAndPassword(auth, email, password).then(() => navigate('/home')).catch(err => alert(err.message))
 
     }
+
+    const registeringHandler = (val) => {
+        setRegistering(val)
+    }
+
+    const registrationHandler = (event) => {
+        event.preventDefault()
+
+        if (registrationInfo.password !== registrationInfo.confirmPassword) {
+            return alert("Password and Confirm Password are not the same.")
+        }
+
+        createUserWithEmailAndPassword(auth, registrationInfo.email, registrationInfo.password).then(() => navigate("/home")).catch(err => alert(err.message))
+    }
+
+
     return (
         <div>
             <h1>Daily Status Report</h1>
             <div>
-                <input type="email" onChange={emailChangeHandler} value={email} />
-                <input type="password" onChange={passwordChangeHandler} value={password} />
-                <button onClick={signInHandler}>Sign In</button>
-                <a href="">Create an account</a>
+                {registering ?
+                    <form onSubmit={registrationHandler}>
+                        <input
+                            type="text"
+                            placeholder="FirstName" />
+                        <input
+                            type="text"
+                            placeholder="LastName" />
+                        <input
+                            type="email"
+                            value={registrationInfo.email}
+                            placeholder="Email"
+                            onChange={(event) => setRegistrationInfo({ ...registrationInfo, email: event.target.value })} />
+                        <input
+                            type="password"
+                            value={registrationInfo.password}
+                            placeholder="Password" onChange={(event) => setRegistrationInfo({ ...registrationInfo, password: event.target.value })}
+
+                        />
+                        <input
+                            type="password"
+                            value={registrationInfo.confirmPassword}
+                            placeholder="Confirm Password"
+                            onChange={(event) => setRegistrationInfo({ ...registrationInfo, confirmPassword: event.target.value })}
+
+                        />
+                        <button>Register</button>
+                        <button onClick={() => registeringHandler(false)}>Back</button>
+                    </form> :
+                    <form onSubmit={signInHandler}>
+
+                        <input
+                            type="email"
+                            onChange={emailChangeHandler}
+                            value={email}
+
+                        />
+                        <input
+                            type="password"
+                            onChange={passwordChangeHandler}
+                            value={password}
+                        />
+                        <button>Sign In</button>
+                        <button onClick={() => registeringHandler(true)}>Create an account</button>
+                    </form>
+                }
             </div>
         </div>
     )
