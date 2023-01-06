44d7178ced09c878aee1cf7f36b736f7a4dab7e1
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-06 15:44:21 +0530

    Sign Out functionalities

--- daily_status_repor/src/pages/Home.jsx
+++ daily_status_repor/src/pages/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { signOut, onAuthStateChanged } from "firebase/auth"
+import { auth } from '../firebase'
+import { useNavigate } from 'react-router-dom'
+
 
 const Home = () => {
-  return (
-    <div>
-        <h1>asdasd</h1>
-    </div>
-  )
+
+    //For navigation
+    const navigate = useNavigate()
+
+    //This hook is used so that user can go to the home page without signing out
+    React.useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (!user) {
+                navigate('/')
+            }
+        })
+    }, [])
+
+    //Sign Out handler function
+    const signOutHandler = () => {
+        signOut(auth).then(() => navigate("/")).catch(err => console.log(err.message))
+    }
+    return (
+        <div>
+            <h1>asdasd</h1>
+            <button onClick={signOutHandler}>Sign Out</button>
+        </div>
+    )
 }
 
 export default Home
--- daily_status_repor/src/pages/Landing.jsx
+++ daily_status_repor/src/pages/Landing.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { signInWithEmailAndPassword ,onAuthStateChanged} from "firebase/auth"
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
 import { auth } from "../firebase.js"
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const Landing = () => {
-    
+
     //Fields for login form
     const [email, setEmail] = React.useState("")
     const [password, setPassword] = React.useState("")
 
     //To make user login persistance
-    React.useEffect(()=>{
-        auth.onAuthStateChanged((user)=>{
-            if(user){
+    React.useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
                 navigate("/home")
             }
         })
-    },[])
+    }, [])
 
-    
-//For navigation
-    const navigate=useNavigate()
+
+    //For navigation
+    const navigate = useNavigate()
 
     //All the handler functions
     const emailChangeHandler = (event) => {
@@ -34,7 +34,7 @@ const Landing = () => {
     const signInHandler = (event) => {
         event.preventDefault();
 
-        signInWithEmailAndPassword(auth, email, password).then(()=>(navigate('/home'))).catch(err => alert(err.message))
+        signInWithEmailAndPassword(auth, email, password).then(() => navigate('/home')).catch(err => alert(err.message))
 
     }
     return (
